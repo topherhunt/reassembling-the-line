9377bb2f4546f0a9fd096939910d6ea4983efc7d
Topher Hunt <hunt.topher@gmail.com>
2018-09-09 13:43:33 +0200

    Rename validation helper param for clarity

--- test/support/validation_helpers.ex
+++ test/support/validation_helpers.ex
@@ -1,14 +1,14 @@
 defmodule EducateYour.ValidationHelpers do
   use ExUnit.CaseTemplate
 
-  def assert_valid(module, changeset_name, default_params, changes \\ %{}) do
-    params = Map.merge(default_params, changes)
+  def assert_valid(module, changeset_name, default_params, overrides \\ %{}) do
+    params = Map.merge(default_params, overrides)
     changeset = apply(module, changeset_name, [struct(module), params])
     assert changeset.valid?, "Expected #{module_name(module)}.#{changeset_name} to be valid, but it's invalid. The changeset: #{inspect(changeset)}"
   end
 
-  def assert_invalid(module, changeset_name, default_params, changes \\ %{}) do
-    params = Map.merge(default_params, changes)
+  def assert_invalid(module, changeset_name, default_params, overrides \\ %{}) do
+    params = Map.merge(default_params, overrides)
     changeset = apply(module, changeset_name, [struct(module), params])
     assert !changeset.valid?, "Expected #{module_name(module)}.#{changeset_name} to be invalid, but it's valid. The changeset: #{inspect(changeset)}"
   end
