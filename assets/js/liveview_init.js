7a9efa8accc6098617fdc58a44ace868f0862f68
Topher Hunt <hunt.topher@gmail.com>
2019-05-05 22:12:39 +0200

    Only init LV socket when necessary

--- assets/js/liveview_init.js
+++ assets/js/liveview_init.js
@@ -1,3 +1,8 @@
 import LiveSocket from "phoenix_live_view";
-let liveSocket = new LiveSocket("/live");
-liveSocket.connect();
+
+// Only connect the LV socket if this page actually contains a LiveView.
+if ($("[data-phx-view]").length > 0) {
+  console.log("Starting LV.");
+  let liveSocket = new LiveSocket("/live");
+  liveSocket.connect();
+}
