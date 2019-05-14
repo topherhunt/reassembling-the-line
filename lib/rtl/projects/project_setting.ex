4989582f2420d68d508db15b641dcaf4c97ad66f
Topher Hunt <hunt.topher@gmail.com>
2019-05-14 22:21:00 +0200

    Fix title on webcam recording page

--- lib/rtl/projects/project_setting.ex
+++ lib/rtl/projects/project_setting.ex
@@ -1,13 +1,9 @@
 defmodule ProjectSetting do
   # Defines the list of valid config fields.
-  # Each field can be of type:
-  #   * string
-  #   * html
-  #   * bool
+  # TODO: The field types don't matter. Make everything a string, and instead define brief custom description / explanation of each field and how to use it.
   def valid_fields do
     %{
-      # TODO: Eventually the logo should be an attached / uploaded file
-      "navbar_logo" => :string,
+      "navbar_logo" => :html,
       "intro_page" => :html,
       "thank_you_page" => :html,
       "show_explore_link" => :bool
