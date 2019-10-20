51cbdbe0d63c6b9c663dffef065071b8da143c69
Topher Hunt <hunt.topher@gmail.com>
2019-10-20 17:43:22 +0200

    Custom blocks: support ADMIN_LOGIN_URL variable

--- lib/rtl_web/views/helpers/custom_block_helpers.ex
+++ lib/rtl_web/views/helpers/custom_block_helpers.ex
@@ -1,13 +1,14 @@
 defmodule RTLWeb.CustomBlockHelpers do
   use Phoenix.HTML
+  alias RTLWeb.Router.Helpers, as: Routes
 
   def custom_block(conn, label) do
+    project = conn.assigns[:project]
     all_labels = RTL.Projects.CustomBlock.templates() |> Enum.map(& &1.label)
     unless label in all_labels, do: raise "Unknown block label: #{inspect(label)}"
 
-    project = conn.assigns[:project]
     if block = find_block(project, label) do
-      raw block.body
+      block.body |> inject_variables(project) |> raw()
     else
       default_block(conn, label)
     end
@@ -16,8 +17,22 @@ defmodule RTLWeb.CustomBlockHelpers do
   defp find_block(nil, _label), do: nil
   defp find_block(proj, label), do: Enum.find(proj.custom_blocks, & &1.label == label)
 
-  # Currently default blocks may inject data from the project record. In the future we
-  # might add more advanced injection support.
+  # We support certain variables for custom blocks.
+  defp inject_variables(body, project) do
+    body =
+      if String.contains?(body, "{ADMIN_LOGIN_URL}") do
+        user = RTL.Accounts.get_user_by!(admin_on_project: project)
+        token = RTL.Accounts.get_login_token(user.email)
+        url = Routes.auth_url(RTLWeb.Endpoint, :confirm, token: token)
+        String.replace(body, "{ADMIN_LOGIN_URL}", url)
+      else
+        body
+      end
+
+    body
+  end
+
+  # Some default blocks inject fields on the assigned project.
   # Note: For some blocks, project may be nil.
   def default_block(conn, label) do
     project = conn.assigns[:project]
--- test/rtl_web/views/custom_block_helper_test.exs
+++ test/rtl_web/views/custom_block_helper_test.exs
@@ -1,5 +1,6 @@
 defmodule RTLWeb.CustomBlockHelperTest do
   use RTLWeb.ConnCase, async: true
+  import RTLWeb.CustomBlockHelpers, only: [custom_block: 2]
 
   test "each available block's default template renders correctly", %{conn: conn} do
     project = Factory.insert_project()
@@ -11,7 +12,20 @@ defmodule RTLWeb.CustomBlockHelperTest do
 
     Enum.each(all_labels, fn label ->
       # Render each custom block to verify that no errors are raised
-      RTLWeb.CustomBlockHelpers.custom_block(conn, label)
+      custom_block(conn, label)
     end)
+  end
+
+  test "variable {ADMIN_LOGIN_URL} works as expected", %{conn: conn} do
+    project = Factory.insert_project()
+    user = Factory.insert_user()
+    RTL.Projects.add_project_admin!(user, project)
+    block = %{label: "landing_page", body: "<a href=\"{ADMIN_LOGIN_URL}\">Login</a>"}
+    project = Map.put(project, :custom_blocks, [block])
+    conn = Plug.Conn.assign(conn, :project, project)
+
+    result = custom_block(conn, "landing_page") |> Phoenix.HTML.safe_to_string()
+
+    assert result =~ ~r(<a href="http://localhost:\d+/auth/confirm\?token=[\w\.\-]+">Login</a>)
   end
 end
