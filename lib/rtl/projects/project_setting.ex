b16b2044b5252a69ea43cadcc554fac5c465c035
Topher Hunt <hunt.topher@gmail.com>
2019-10-24 13:22:50 +0200

    Remove deprecated ProjectSettings

--- lib/rtl/projects/project_setting.ex
+++ lib/rtl/projects/project_setting.ex
@@ -8,15 +8,7 @@ defmodule RTL.Projects.ProjectSetting do
       "recording_permission_options" => "JSON for the webcam recording permission options. Must be an object where each key is the label display text and each value is the setting to store.",
 
       # This setting will likely stay, though it might be renamed.
-      "show_explore_link" => "\"true\" if the link to explore project results should be visible in the navbar.",
-
-      # Will be made redundant once we implement CustomBlocks
-      "logo_image_url" => "URL for the logo that should be shown in the navbar next to the project name (if any).",
-      "logo_link_url" => "Where should the user be directed when they click on the logo? (by default, the logo won't link anywhere)",
-      "project_intro_page" => "Text / HTML for the project information page that any participant will see when they click on the project name / logo in the navbar.",
-      "webcam_recording_intro" => "Text / HTML shown at the top of the webcam recording page, incl. the h1 title.",
-      "recording_consent_text" => "Text / HTML for the webcam recording page consent box.",
-      "thank_you_page" => "Text / HTML shown to the participant after they submit a recording."
+      "show_explore_link" => "\"true\" if the link to explore project results should be visible in the navbar."
     }
   end
 end
--- test/rtl_web/controllers/manage/project_setting_controller_test.exs
+++ test/rtl_web/controllers/manage/project_setting_controller_test.exs
@@ -9,7 +9,7 @@ defmodule RTLWeb.Manage.ProjectSettingControllerTest do
       # It rejects me even if I'm an admin
       Projects.add_project_admin!(user, p)
 
-      conn = get(conn, Routes.manage_project_setting_path(conn, :edit, p, "project_intro_page"))
+      conn = get(conn, Routes.manage_project_setting_path(conn, :edit, p, "show_explore_link"))
 
       assert redirected_to(conn) == Routes.home_path(conn, :index)
       assert conn.halted
@@ -33,7 +33,7 @@ defmodule RTLWeb.Manage.ProjectSettingControllerTest do
       conn = get(conn, edit_path(conn, project))
 
       assert conn.resp_body =~ "test-manage-project-setting-edit-path"
-      assert conn.resp_body =~ "project_intro_page"
+      assert conn.resp_body =~ "show_explore_link"
     end
   end
 
@@ -44,7 +44,7 @@ defmodule RTLWeb.Manage.ProjectSettingControllerTest do
 
       conn = patch(conn, update_path(conn, project), %{"value" => "new value"})
 
-      assert Projects.get_project!(project.id).settings["project_intro_page"] == "new value"
+      assert Projects.get_project!(project.id).settings["show_explore_link"] == "new value"
       assert redirected_to(conn) == Routes.manage_project_path(conn, :show, project)
     end
 
@@ -60,10 +60,10 @@ defmodule RTLWeb.Manage.ProjectSettingControllerTest do
   end
 
   defp edit_path(conn, project) do
-    Routes.manage_project_setting_path(conn, :edit, project, "project_intro_page")
+    Routes.manage_project_setting_path(conn, :edit, project, "show_explore_link")
   end
 
   defp update_path(conn, project) do
-    Routes.manage_project_setting_path(conn, :update, project, "project_intro_page")
+    Routes.manage_project_setting_path(conn, :update, project, "show_explore_link")
   end
 end
